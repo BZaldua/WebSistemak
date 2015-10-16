5f2c6df99e57cc8cbf1b4a6f03601f50a4e9633a
Raul Medeiros <rmedeirosperez@gmail.com>
2015-10-16 14:27:54 +0200

    errorearekin

--- signIn.php
+++ signIn.php
@@ -12,8 +12,17 @@ echo "<!DOCTYPE html>
 		</head>
 		<body>";
 		//Egiteko
+<<<<<<< HEAD
+	$link =  new mysqli("localhost","root","","quiz");
+	if($link->mysqli_connect_errno) 
+	{
+		die( "Huts egin du konexioak MySQL-ra: (". 
+		 $link->connect_errno() . ") " . 
+		 $link->connect_error()	);
+=======
 	$link = new mysqli_connect("localhost","root","","quiz");
 	if()
+>>>>>>> 549616c9388d21209a49c2b57fa86a01f7baeffb
 
 	echo" <h4>Sign In</h4>
 	<form id='LogIn' name='LogIn' method='post'>
@@ -22,6 +31,24 @@ echo "<!DOCTYPE html>
 		<button	type='submit' value='submit'>Submit</button>
 	</form>";		
 
+<<<<<<< HEAD
+	if($erabiltzaileak=$link->query("Select* from erabiltzaile where email=$_POST['email'] and pasahitza=$_POST['pasahitza']")){
+		echo “Taularen sorrerak huts egin du: (" . 
+		$link->errno . ") " . $link->error"; 
+	}
+	if(mysql_fetch_lengths($erabilzaileak)==1){
+		echo"<a href=''>Quizzes<a/>"
+		
+	}else{
+		echo"<p color='red'></p>";
+	}
+	
+	
+	
+	
+	
+=======
+>>>>>>> 549616c9388d21209a49c2b57fa86a01f7baeffb
 echo"</body>
 	</html>"
 ?>
