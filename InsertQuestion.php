cb943444b7a42e492abc7edb61c287d752ee773f
Beñat Zaldua <benatzaldua1775@gmail.com>
2015-10-29 16:00:55 +0100

    Labo 5 v2

--- InsertQuestion.php
+++ InsertQuestion.php
@@ -1,8 +1,8 @@
 <?php 
 session_start();
 
-	//$link = new mysqli("localhost","root","","quiz");	
-	$link = new mysqli("mysql.hostinger.es","u526113874_rb15","123456789","u526113874_quiz");
+	$link = new mysqli("localhost","root","","quiz");	
+	//$link = new mysqli("mysql.hostinger.es","u526113874_rb15","123456789","u526113874_quiz");
 	if($link->connect_errno) {
 		die( "Huts egin du konexioak MySQL-ra: (". 
 		$link->connect_errno() . ") " . 
@@ -21,14 +21,15 @@ session_start();
 		$galdera = isset($_POST['galdera']) ? $_POST['galdera'] : '';
 		$erantzuna = isset($_POST['erantzuna']) ? $_POST['erantzuna'] : '';
 		$zailtasuna = isset($_POST['zailtasuna']) ? $_POST['zailtasuna'] : '';
+		$gaia = isset($_POST['gaia']) ? $_POST['gaia'] : '';		
 		
-		
-		$sql = "INSERT INTO galdera (galdera, erantzuna, zailtasuna, emaila) VALUES ('$galdera','$erantzuna','$zailtasuna','$emaila')";
+		$sql = "INSERT INTO galdera (galdera, erantzuna, zailtasuna, emaila, gaia) VALUES ('$galdera','$erantzuna','$zailtasuna','$emaila', '$gaia')";
 		if (!$link -> query($sql)){
 			die("<p>An error happened: ".$link -> error."</orp>");
 		}else{
 				$assessmentItem= $xml-> addChild('assessmentItem');
 				$assessmentItem-> addAttribute('konplexutasuna',$zailtasuna);
+				$assessmentItem -> addAttribute ('gaia',$gaia);
 				$itemBody= $assessmentItem ->addChild('itemBody');
 				$itemBody->addChild('p',$galdera);
 				$correctResponse= $assessmentItem-> addChild('correctResponse');
@@ -94,7 +95,9 @@ session_start();
 			<option value="3">3</option>
 			<option value="4">4</option>
 			<option value="5">5</option>
-		</select><br/><br/>
+		</select><br><br>
+		<label>Topic</label><br>
+		<input type="text" id ="gaia" name="gaia" required><br><br>
 		<button type='submit' name='bidali'>Submit</button>
 	</form>
 	</center>
