eedafa757e04e21cfc5030e864e7b272b9c6a519
Beñat Zaldua <benatzaldua1775@gmail.com>
2015-10-30 14:15:07 +0100

    Irakurri xml

--- seeXMLQuestions.php
+++ seeXMLQuestions.php
@@ -1,3 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>XML Bistaratu</title>
+	<meta name="tipo_contenido" content="text/html;" http-equiv="content-type" charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesPWS/estiloa.css"/>
+		<link rel='stylesheet' 
+		   type='text/css' 
+		   media='only screen and (max-width: 480px)'
+		   href='stylesPWS/smartphone.css' />
+</head>
+<body>
+<div class ="nav" id="nav">
+			<ul>
+				<li class ="ezkerra"><a href ="layout.html">Home</a></li>
+				<li class ="ezkerra"><a href ="credits.html">Credits</a></li>
+				<li class ="ezkerra"><a href ="quiz.php">Quizzes</a></li>
+			</ul>
+			<ul>
+				<li class ="eskuina"><a href ="login.php">Log In</a></li>
+			</ul>
+		</div>
+		<hr>
+		<center><h1>XML Fitxategia </h1><center>
+	<table id ="taula">
+		<tr>
+			<th>Enuntziatua</th>
+			<th>Konplexutasuna</th>
+			<th>Gaia</th>
+		</tr>
 <?php
-
+	$xml = simplexml_load_file("galderak.xml");
+	foreach($xml ->children() as $galdera){
+		$konplexutasuna = $galdera[0]['konplexutasuna'];
+		$gaia = $galdera[0]['gaia'];
+		$enuntziatua = $galdera[0] -> itemBody -> p;
+		echo "<tr><td>".$enuntziatua."</td>";
+		echo "<td style= 'text-align:center'>". $konplexutasuna."</td>";
+		echo "<td style= 'text-align:center'>". $gaia. "</td></tr>";
+	}
 ?>
+	</table>
+</body>
+</html>
