4bb42eef6da62c54521215033011992767581468
Beñat Zaldua <benatzaldua1775@gmail.com>
2015-11-23 17:25:52 +0100

    SimpleReg erregistratu. Probatu gabe

--- login.php
+++ login.php
@@ -1,69 +1,62 @@
 <?php
-session_start();
- 
-$link = new mysqli("localhost","root","","quiz");
-//$link = new mysqli("mysql.hostinger.es","u526113874_rb15","123456789","u526113874_quiz");
-if($link->connect_errno) {
-		die( "Huts egin du konexioak MySQL-ra: (". 
-		$link->connect_errno() . ") " . 
-		$link->connect_error()	);
-}
- 
-if(isset($_SESSION["session_username"])){
-header("Location: handlingQuizzes.php");
-}
- 
-if(isset($_POST["login"])){
- 
-	if(!empty($_POST['emaila']) && !empty($_POST['password'])) {
-		$user=$_POST['emaila'];
-		$password=$_POST['password'];
+	session_start();
 	 
-		$erabiltzaileak =$link -> query("SELECT * FROM erabiltzaile WHERE email='".$user."' AND pasahitza='".$password."'");
-	 
-		$numrows = mysqli_num_rows($erabiltzaileak);
-	 if($numrows!=0){
-		while($row = mysqli_fetch_assoc($erabiltzaileak))
-		{
-		$user2=$row['email'];
-		$pass2=$row['pasahitza'];
-		}
-	 
-
-	if($user == $user2 && $password == $pass2){
-	 
-	 
-	 $_SESSION['session_username']=$user;
-	 $ordua= Date('H:i:s');
-
-	 $konexioa=$link-> query("SELECT emaila FROM konexioak WHERE emaila=$user");
-		 $konkop= mysqli_num_rows($konexioa);
-		if($konkop==0){
-			$sql= "INSERT INTO konexioak(emaila,ordua) values ('$user','$ordua')";
-		}else{
-			$sql="UPDATE konexioak SET ordua='$ordua' WHERE emaila='$user'";	
-		}
-	 if (!$link -> query($sql)){
-				die("<p>An error happened: ".$link -> error()."</p>");
-			}
-		if($_SESSION['session_username']=='web000@ehu.es'){
-			header("Location: reviewingQuizzes.php");
-		}else{
-			/* Redirect browser */
-			header("Location: handlingQuizzes.php");
-		}
-	 
-	 } else {
-	 
-	$message = "Wrong email or password";
-	 }
-	 
-	} else {
-	 $message = "Something went wrong";
+	$link = new mysqli("localhost","root","","quiz");
+	//$link = new mysqli("mysql.hostinger.es","u526113874_rb15","123456789","u526113874_quiz");
+	if($link->connect_errno) {
+			die( "Huts egin du konexioak MySQL-ra: (". 
+			$link->connect_errno() . ") " . 
+			$link->connect_error()	);
 	}
-	
-}
-}
+	 
+	if(isset($_SESSION["session_username"])){
+		header("Location: handlingQuizzes.php");
+	}
+	 
+	if(isset($_POST["login"])){
+	 
+		if(!empty($_POST['emaila']) && !empty($_POST['password'])) {
+			$user=$_POST['emaila'];
+			$password=$_POST['password'];
+			
+			$erabiltzaileak =$link -> query("SELECT * FROM erabiltzaile WHERE email='".$user."' AND pasahitza='".$password."'");
+		 
+			$numrows = mysqli_num_rows($erabiltzaileak);
+			if($numrows!=0){
+				while($row = mysqli_fetch_assoc($erabiltzaileak)){
+					$user2=$row['email'];
+					$pass2=$row['pasahitza'];
+				}
+				if($user == $user2 && $password == $pass2){
+					$_SESSION['session_username']=$user;
+					$_SESSION['loginCount']++;
+					$ordua= Date('H:i:s');
+					$konexioa=$link-> query("SELECT emaila FROM konexioak WHERE emaila=$user");
+					$konkop= mysqli_num_rows($konexioa);
+					if($konkop==0){
+						$sql= "INSERT INTO konexioak(emaila,ordua) values ('$user','$ordua')";
+					}else{
+						$sql="UPDATE konexioak SET ordua='$ordua' WHERE emaila='$user'";	
+					}
+		 
+					if (!$link -> query($sql)){
+						die("<p>An error happened: ".$link -> error()."</p>");
+					}
+					if($_SESSION['session_username']=='web000@ehu.es'){
+						header("Location: reviewingQuizzes.php");
+					}else{
+						/* Redirect browser */
+						header("Location: handlingQuizzes.php");
+					}
+				} else {
+				$message = "Wrong email or password";
+				}
+		 
+			} else {
+				$message = "You tried 3 times.";
+			}
+		}
+	}
 ?>
 
 <!DOCTYPE html>
