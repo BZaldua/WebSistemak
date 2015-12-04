a064bf25277f840937b1f0dc681cf831fb954bd4
Beñat Zaldua <benatzaldua1775@gmail.com>
2015-12-04 09:23:10 +0100

    Errore zuzenketa

--- sesioEgoera.php
+++ sesioEgoera.php
@@ -3,6 +3,6 @@ session_start();
 	if(!isset($_SESSION['session_username'])){
 		echo "<a href='login.php'>Log In</a>";
 	}else{
-		echo "<a href='logOut.php'>Log Out</a>";
+		echo "<a href='logout.php'>Log Out</a>";
 	}
 ?>
